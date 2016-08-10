2f52349d9d47013f4f2bae22447c2bff0feb73cb
Mihir Joshi <mihir3535@gmail.com>
2016-08-09 20:12:44 -0400

    Add initial code + requirements.txt

---
+++ analysis/Summarizer.py
@@ -0,0 +1,45 @@
+from sumy.nlp.stemmers import Stemmer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.utils import get_stop_words
+
+LANG = 'english'
+SENTENCE_COUNT = 7
+
+
+def summarize_text(textbody):
+    parser = PlaintextParser.from_string(textbody, Tokenizer(LANG))
+    stemmer = Stemmer(LANG)
+
+    summarizer = LsaSummarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANG)
+
+    summary = summarizer(parser.document, SENTENCE_COUNT)
+
+    summarized_text = ''
+    for sentence in summary:
+        summarized_text += str(sentence) + ' '
+
+    return summarized_text
+
+
+def main():
+    print(summarize_text(
+        "Ok. So. I am a bartender at a pretty popular place where I live. Last night I worked and after work around"
+        " midnight I'm rolling silverware and having an after work beer. The barback walks up to me and says"
+        " \"Hey, customers keep telling me there has been someone in the women's restroom for almost an hour."
+        " I am worried someone is passed out in there, can you check it for me?\" Just so you know it is a one "
+        "stall bathroom and that exact scenario has happened before. So I go over and I'm pounding on the door and no"
+        " one answers. I start to panic. So I start pounding harder and eventually slam my shoulder into the door and"
+        " bash the door open. Wood splinters from the door frame go flying. There is a girl on the toilet TAKING A"
+        " DUMP. She turns to me and in the most classic valley girl voice says, \"Seriously?\" So I'm babbling and"
+        " apologizing profusely saying oh man I work here blah blah blah. Super embarrassing. Then I come out of the"
+        " bathroom area to tell my co workers carrying shard of wood with me and they tell me \"You know there\'s a"
+        " KEY TO THE BATHROOM RIGHT?\" I. Am. So. Embarrassed. I'll never live it down. "
+    )
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ main/main.py
@@ -0,0 +1,81 @@
+import logging
+import validators
+
+from newspaper import Article
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from analysis import Summarizer
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define command handlers.
+def bot_start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi, thanks for using SummarizerBot!\n\n'
+                                                 'Please provide me with a link to your news article and I will '
+                                                 'do my best to summarize it for you!')
+
+
+def bot_help(bot, update):
+    bot.sendMessage(update.message.chat_id, text="/help -- this help message\n"
+                                                 "/summarize [URL] -- returns a summary of the article provided")
+
+
+def bot_error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def summarize(bot, update):
+    user_message = str(update.message.text)
+    if(user_message.startswith('/summarize')):
+        user_message = user_message.split(' ',1)[1]
+    logger.info(user_message)
+    if not validators.url(user_message):
+        bot.sendMessage(update.message.chat_id,
+                        text="Unfortunately, I can't summarize something that is not a URL. Send me an article URL and I'll try to summarize it!")
+    else:
+        article = Article(user_message)
+        article.download()
+        article.parse()
+
+        # if not article.is_valid_url(): bot.sendMessage(update.message.chat_id, text="Couldn't parse article!")
+
+        bot.sendMessage(update.message.chat_id, text=Summarizer.summarize_text(article.text))
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+
+    updater = Updater("241750668:AAF361s9RNfOTl94U9jQKTIKA8apsXP5oMo")
+
+    # Get the dispatcher to register handlers
+
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+
+    dp.add_handler(CommandHandler("start", bot_start))
+
+    dp.add_handler(CommandHandler("help", bot_help))
+
+    dp.add_handler(CommandHandler("summarize", summarize))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler([Filters.text], summarize))
+
+    # log all errors
+    dp.add_error_handler(bot_error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
